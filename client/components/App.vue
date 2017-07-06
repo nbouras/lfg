fe937414d3008415766563914c966185c3b44fce
nikolaos <nikolaos.bouras@outlook.com>
2017-07-06 22:37:52 +0200

    Remove styling from Home componenet

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,7 +1,7 @@
 <template>
-
+  <!-- This the root element. Everything must be inside-->
   <div id="app">
-    <p> Visible in every page </p>
+
 
     <!-- This loads the component that matches the url as configured in router/index.js-->
     <router-view></router-view>
@@ -9,24 +9,10 @@
   </div>
 </template>
 
+
+<script>
+
+</script>
 <style>
-body {
-  margin: 0;
-  font-size: 2rem;
-  font-family: -apple-system, BlinkMacSystemFont,
-               'avenir next', avenir,
-               helvetica, 'helvetica neue',
-               Ubuntu,
-               'segoe ui', arial,
-               sans-serif;
-}
-.page {
-  text-align: center;
-  /* nesting for the need to test postcss */
-  code {
-    background-color: #f0f0f0;
-    padding: 3px 5px;
-    border-radius: 2px;
-  }
-}
+
 </style>
