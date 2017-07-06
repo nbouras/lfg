56fe5a47310c33f61e6093cbcea6f491f68caccb
nikolaos <nikolaos.bouras@outlook.com>
2017-07-06 22:38:23 +0200

    Add component Navigation

---
+++ client/components/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-normal navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#">
+                    <i class="fa fa-play-circle"></i> <span class="light">LFG</span>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                      <li v-for="menu in menus" >
+                      <a v-bind:href=menu.initialURL> {{menu['title']}} </a>
+
+                      </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+</template>
+
+<script>
+
+  module.exports = {
+
+   data: function(){
+       return{
+          menus: [
+            {'title':'dashboard', 'initialURL': '/dashboard'},
+            {'title':'modules', 'initialURL': '/modules'},
+            {'title':'transactions', 'initialURL': '/transactions'},
+            {'title':'roles', 'initialURL': '/roles'},
+            {'title':'users', 'initialURL': '/users'},
+            {'title':'accessRights', 'initialURL': '/accessrights'},
+            {'title':'help', 'initialURL': '/help'}
+            ]
+       }
+   }
+}
+
+</script>
+
+<style>
+
+</style>
