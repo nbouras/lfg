5d5a954ea02e53a683496a26c13a9f36f4f8ac7c
nikolaos <nikolaos.bouras@outlook.com>
2017-09-15 19:11:26 +0200

    Split fronted and backend to directories.
    Use Python setuptools

---
+++ backend/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(
+    name="LFG",
+    version="0.0.1",
+    author="Nikolaos Bouras",
+    author_email="nikolaos.bouras@outlook.com",
+    description=("Web tool to help creating groups."),
+    license = "BSD",
+
+    install_requires=['Flask>=0.12.0'],
+
+
+)
